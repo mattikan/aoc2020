11358713c9c0c03f5c35ceaf8ba9118483e8b124
Matti Känninen <mattikan@cs.helsinki.fi>
2020-12-23 14:01:37 +0200

    day 8 b

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,23 +1,50 @@
 use std::{collections::HashMap, fs};
 
 fn main() {
-    let lines: Vec<String> = fs::read_to_string("input")
+    let mut lines: Vec<String> = fs::read_to_string("input")
         .unwrap()
         .trim()
         .split("\n")
         .map(|s| String::from(s))
         .collect();
 
+    check(&lines);
+    for i in 0..lines.len() {
+        let line = lines.get(i).unwrap().to_string();
+        if line.contains("nop") {
+            lines[i] = line.replace("nop", "jmp").to_string();
+            if check(&lines) {
+                return;
+            }
+            lines[i] = line.replace("jmp", "nop").to_string();
+        }
+        if line.contains("jmp") {
+            lines[i] = line.replace("jmp", "nop").to_string();
+            if check(&lines) {
+                return;
+            }
+            lines[i] = line.replace("nop", "jmp").to_string();
+        }
+    }
+}
+
+fn check(lines: &Vec<String>) -> bool {
     let mut seen: HashMap<i32, bool> = HashMap::new();
     let mut acc = 0;
     let mut line = 0;
     loop {
         if !seen.get(&line).is_none() {
             println!("we've seen line {} before, acc is {}", &line, &acc);
-            break;
+            return false;
         }
         seen.insert(line, true);
-        let command = lines.get(line as usize).unwrap();
+        let command = lines.get(line as usize);
+        if command.is_none() {
+            println!("reached the end! acc is {}", acc);
+            return true;
+        }
+
+        let command = command.unwrap();
         if command.contains("nop") {
             line = line + 1;
             continue;
